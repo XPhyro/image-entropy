582b3f8368285a72d2f3e2216c9280603173fe0c
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2021-11-02 13:55:41 +0300

    Implement conic-gradient test image

--- src/testimages.py
+++ src/testimages.py
@@ -9,7 +9,13 @@ def gradlinearv(w=1024, h=1024):
     return np.outer(np.arange(256), np.ones(256)).astype(np.uint8)
 
 
+def gradconic(w=1024, h=1024):
+    x, y = np.meshgrid(range(w), range(h))
+    return (np.arctan2(h / 2 - y, w / 2 - x) * (255 / np.pi / 2)).astype(np.uint8)
+
+
 strtofunc = {
     "horizontal-linear-gradient": gradlinearh,
     "vertical-linear-gradient": gradlinearv,
+    "conic-gradient": gradconic,
 }
