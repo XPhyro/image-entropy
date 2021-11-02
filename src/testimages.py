311e027f0ff206bbc2b38fa6430ed464c7744ab1
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2021-11-02 14:07:49 +0300

    Implement uniform-random test image

--- src/testimages.py
+++ src/testimages.py
@@ -14,8 +14,13 @@ def gradconic(w=1024, h=1024):
     return (np.arctan2(h / 2 - y, w / 2 - x) * (255 / np.pi / 2)).astype(np.uint8)
 
 
+def randunif(w=1024, h=1024):
+    return (np.random.rand(w, h) * 255).astype(np.uint8)
+
+
 strtofunc = {
     "horizontal-linear-gradient": gradlinearh,
     "vertical-linear-gradient": gradlinearv,
     "conic-gradient": gradconic,
+    "uniform-random": randunif,
 }
