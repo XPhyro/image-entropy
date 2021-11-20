796dbcd45cb89be4263b416eb909ee1a316e6350
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2021-11-20 22:47:52 +0300

    Document `benchmark` and `log` modules

--- src/benchmark.py
+++ src/benchmark.py
@@ -13,7 +13,35 @@ except:
     haspapi = False
 
 
-def checkevent(event):
+def _checkevent(event):
+    """
+    Check for the existence of PAPI events.
+
+    This function checks for the existence of the given PAPI event by starting and
+    stopping a counter on it. Unless you know what you are doing, do not use this
+    directly; instead, use ``benchmark``.
+
+    Parameters
+    ----------
+    event : int
+        Event to check.
+
+    Returns
+    -------
+    bool
+        Whether the event exists.
+
+    See Also
+    --------
+    benchmark : TODO
+
+    Examples
+    --------
+    >>> _checkevent(events.PAPI_FP_OPS)
+    False
+    >>> _checkevent(events.PAPI_SP_OPS)
+    True
+    """
     try:
         high.start_counters([event])
         high.stop_counters()
@@ -22,8 +50,40 @@ def checkevent(event):
         return False
 
 
-def measurepapi(event, count, func, funcargs):
-    if not checkevent(event):
+def _measurepapi(event, count, func, funcargs):
+    """
+    Measure the given event for the given function and arguments for count times.
+
+    Unless you know what you are doing, do not use this directly; instead, use ``benchmark``.
+
+    Parameters
+    ----------
+    event : int
+        Event to measure.
+    count : int
+        Number of times the measurement should be done.
+    func : function
+        Function to measure on.
+    funcargs : tuple
+        Arguments to pass to the function.
+
+    Returns
+    -------
+    int
+        Mean of measured event count.
+
+    See Also
+    --------
+    benchmark : TODO
+
+    Examples
+    --------
+    >>> _measurepapi(events.PAPI_FP_OPS, 10, methods.method, (args, colourimg, greyimg))
+    234567
+    >>> _measurepapi(events.PAPI_DP_OPS, 10, methods.method, (args, colourimg, greyimg))
+    123456
+    """
+    if not _checkevent(event):
         return -1
 
     measure = 0
@@ -37,6 +97,37 @@ def measurepapi(event, count, func, funcargs):
 
 
 def benchmark(args, func, funcargs):
+    """
+    Benchmark the given function and arguments using various events.
+
+    Benchmark the given functions, passing the given arguments to it, using various
+    events. The events used are: PAPI_FP_OPS, PAPI_SP_OPS, PAPI_DP_OPS, PAPI_VEC_sP, PAPI_VEC_SP, PAPI_REF_CYC.
+
+    Parameters
+    ----------
+    args : Namespace
+        The return value of argparse.ArgumentParser(...).parse_args(...).
+    func : function
+        Function to benchmark.
+    funcargs : tuple
+        Arguments to pass to the function.
+
+    Returns
+    -------
+    None
+
+    Examples
+    --------
+    >>> benchmark(args, methods.method, (args, colourimg, greyimg))
+    FLO................: 234567
+    VO.................: 123456
+    FLOPS..............: 23456789
+    VOPS...............: 12345678
+    FLOPC..............: 0.912356
+    VOPC...............: 0.812345
+    process time.......: 1.123456
+    processor cycles...: 12345678
+    """
     if not args.print_performance:
         return
 
@@ -56,15 +147,15 @@ def benchmark(args, func, funcargs):
 
         count = args.performance_count
 
-        if checkevent(events.PAPI_FP_OPS):
-            fp = measurepapi(events.PAPI_FP_OPS, count, func, funcargs)
-        elif checkevent(events.PAPI_SP_OPS) and checkevent(events.PAPI_DP_OPS):
+        if _checkevent(events.PAPI_FP_OPS):
+            fp = _measurepapi(events.PAPI_FP_OPS, count, func, funcargs)
+        elif _checkevent(events.PAPI_SP_OPS) and _checkevent(events.PAPI_DP_OPS):
             log.warn(
                 "PAPI_FP_OPS unavailable. "
                 + "FLO/FLOPS will be estimated using PAPI_SP_OPS and PAPI_DP_OPS."
             )
 
-            fp = measurepapi(events.PAPI_DP_OPS, count, func, funcargs) + measurepapi(
+            fp = _measurepapi(events.PAPI_DP_OPS, count, func, funcargs) + _measurepapi(
                 events.PAPI_SP_OPS, count, func, funcargs
             )
         else:
@@ -72,10 +163,10 @@ def benchmark(args, func, funcargs):
 
         vp = max(
             -1,
-            measurepapi(events.PAPI_VEC_DP, count, func, funcargs)
-            + measurepapi(events.PAPI_VEC_SP, count, func, funcargs),
+            _measurepapi(events.PAPI_VEC_DP, count, func, funcargs)
+            + _measurepapi(events.PAPI_VEC_SP, count, func, funcargs),
         )
-        cyc = measurepapi(events.PAPI_REF_CYC, count, func, funcargs)
+        cyc = _measurepapi(events.PAPI_REF_CYC, count, func, funcargs)
 
         cputime = time.process_time()
         func(*funcargs)
--- src/log.py
+++ src/log.py
@@ -9,6 +9,35 @@ errenabled = True
 
 
 def info(*msgs):
+    """
+    Log informative messages.
+
+    This function logs the given objects to stdout. It prepends each object with the
+    executable name (``execname``), appends a newline, converts the objects to strings
+    and prints them. The logging does not happen if ``infoenabled`` is not True.
+
+    Parameters
+    ----------
+    *msgs : object
+        Messages to log.
+
+    Returns
+    -------
+    None
+
+    See Also
+    --------
+    warn : Log warning messages.
+    err : Log error messages.
+
+    Examples
+    --------
+    >>> info("informative message")
+    execname: informative message
+    >>> info("informative message 1", "informative message 2")
+    execname: informative message 1
+    execname: informative message 2
+    """
     if not infoenabled:
         return
 
@@ -17,6 +46,35 @@ def info(*msgs):
 
 
 def warn(*msgs):
+    """
+    Log warning messages.
+
+    This function logs the given objects to stdout. It prepends each object with the
+    executable name (``execname``), appends a newline, converts the objects to strings
+    and prints them. The logging does not happen if ``warnenabled`` is not True.
+
+    Parameters
+    ----------
+    *msgs : object
+        Messages to log.
+
+    Returns
+    -------
+    None
+
+    See Also
+    --------
+    info : Log informative messages.
+    err : Log error messages.
+
+    Examples
+    --------
+    >>> info("warning message")
+    execname: warning message
+    >>> info("warning message 1", "warning message 2")
+    execname: warning message 1
+    execname: warning message 2
+    """
     if not warnenabled:
         return
 
@@ -25,6 +83,35 @@ def warn(*msgs):
 
 
 def err(*msgs):
+    """
+    Log error messages.
+
+    This function logs the given objects to stdout. It prepends each object with the
+    executable name (``execname``), appends a newline, converts the objects to strings
+    and prints them. The logging does not happen if ``errenabled`` is not True.
+
+    Parameters
+    ----------
+    *msgs : object
+        Messages to log.
+
+    Returns
+    -------
+    None
+
+    See Also
+    --------
+    info : Log informative messages.
+    warn : Log warning messages.
+
+    Examples
+    --------
+    >>> info("error message")
+    execname: error message
+    >>> info("error message 1", "error message 2")
+    execname: error message 1
+    execname: error message 2
+    """
     if not errenabled:
         return
 
