e7d34932d834bbe738b1636ab98f75b3cbd3ec2d
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2022-07-21 15:28:12 +0300

    Extract constants to module, add comments and format code in random/

--- src/random/main.py
+++ src/random/main.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
+# References:
+#   [1]: linux/drivers/char/random.c
 
 
 import hashlib
 
 import chacha
-
-
-TESTKEY = [0x31] * 16
-TESTIV = [0x41] * 8
+from constants import TESTKEY, TESTIV
 
 
 def main():
     buf = [0x59] * 64
     cipher = chacha.ChaCha(TESTKEY, TESTIV)
+
+    # hash to not expose internal states, see [1]
     sha = hashlib.sha256()
-    assert sha.digest_size < len(buf)  # see linux/drivers/char/random.c
+    assert sha.digest_size < len(buf)  # see [1]
 
     for _ in range(10):
         sha.update(bytes(cipher.next(buf)))
