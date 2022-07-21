15ca1b6e098426cd5fa67c3cefac57009685d983
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2022-07-21 15:03:55 +0300

    Output hashed random number to not give away internal states
    See linux/drivers/char/random.c

--- src/random/main.py
+++ src/random/main.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
+
+import hashlib
+
 import chacha
+
 
 TESTKEY = [0x31] * 16
 TESTIV = [0x41] * 8
@@ -9,13 +13,12 @@ TESTIV = [0x41] * 8
 def main():
     buf = [0x59] * 64
     cipher = chacha.ChaCha(TESTKEY, TESTIV)
+    sha = hashlib.sha256()
+    assert sha.digest_size < len(buf)  # see linux/drivers/char/random.c
 
     for _ in range(10):
-        randarr = cipher.next(buf)
-        rand = 0
-        for i, r in enumerate(randarr):
-            rand += 2**i * r
-
+        sha.update(bytes(cipher.next(buf)))
+        rand = sha.hexdigest()
         print(rand)
 
 
