c0543a702a5fc8608a1caa371b1a67026b2211bf
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2022-07-25 18:17:28 +0300

    Implement subcommand system in run.sh

--- run.sh
+++ run.sh
@@ -8,81 +8,92 @@ logerrq() {
     exit 1
 }
 
-optfilter=1
-unset pythonpath optmodel
-while getopts "ahP:ps" OPT; do
-    case "$OPT" in
-        a) optmodel="ade20k";;
-        h)
-            printf "%s" \
-"usage: $0 [-a] [-h] [-m] [-P] [-p] [-s] [ARG [ARG ...]] -- [ARG [ARG ...]]
+segmentation() {
+    optfilter=1
+    unset pythonpath optmodel
+    while getopts "ahP:ps" OPT; do
+        case "$OPT" in
+            a) optmodel="ade20k";;
+            h)
+                printf "%s" \
+    "usage: $0 [-a] [-h] [-m] [-P] [-p] [-s] [ARG [ARG ...]] -- [ARG [ARG ...]]
 
-Quickly run the Python project using pre-set arguments.
+    Quickly run the Python project using pre-set arguments.
 
-   -a        enable Ade20k semantic segmentation
-   -h        show this help message and exit
-   -P PATH   path to Python executable
-   -p        enable Pascalvoc semantic segmentation
-   -s        do not filter output
-"
-            exit 0
-            ;;
-        P) pythonpath="$OPT";;
-        p) optmodel="pascalvoc";;
-        s) optfilter=0;;
-        *) printf "Invalid option given: %s\n" "$OPT"; exit 1;;
-    esac
-done
-shift "$((OPTIND - 1))"
+       -a        enable Ade20k semantic segmentation
+       -h        show this help message and exit
+       -P PATH   path to Python executable
+       -p        enable Pascalvoc semantic segmentation
+       -s        do not filter output
+    "
+                exit 0
+                ;;
+            P) pythonpath="$OPT";;
+            p) optmodel="pascalvoc";;
+            s) optfilter=0;;
+            *) printf "Invalid option given: %s\n" "$OPT"; exit 1;;
+        esac
+    done
+    shift "$((OPTIND - 1))"
 
-[ -z "$optmodel" ] && logerrq "One of -a, -m or -p must be given.\n"
+    [ -z "$optmodel" ] && logerrq "One of -a, -m or -p must be given.\n"
 
-if [ "$optfilter" -ne 0 ]; then
-    if command -v afgrep > /dev/null 2>&1; then
-        filter() {
-            afgrep -a 'main.py ['
-        }
+    if [ "$optfilter" -ne 0 ]; then
+        if command -v afgrep > /dev/null 2>&1; then
+            filter() {
+                afgrep -a 'main.py ['
+            }
+        else
+            printf "%s\n" "afgrep (from XPhyro/scripts) not found, falling back to grep." >&2
+            filter() {
+                grep '^main.py \['
+            }
+        fi
     else
-        printf "%s\n" "afgrep (from XPhyro/scripts) not found, falling back to grep." >&2
         filter() {
-            grep '^main.py \['
+            cat
         }
     fi
-else
-    filter() {
-        cat
-    }
-fi
 
-[ -n "$pythonpath" ] \
-    || pythonpath="$(command -v python3.7 2> /dev/null)" \
-    || pythonpath="$(command -v python3 2> /dev/null)" \
-    || pythonpath="$(command -v python 2> /dev/null)" \
-    || logerrq "Could not find a suitable executable for Python. Supply one with -p option.\n"
+    [ -n "$pythonpath" ] \
+        || pythonpath="$(command -v python3.7 2> /dev/null)" \
+        || pythonpath="$(command -v python3 2> /dev/null)" \
+        || pythonpath="$(command -v python 2> /dev/null)" \
+        || logerrq "Could not find a suitable executable for Python. Supply one with -p option.\n"
 
-if [ "$#" -ne 0 ]; then
-    perf stat unbuffer \
-        "$pythonpath" src/development/main.py "$@"
-else
-    case "$optmodel" in
-        ade20k)
-            opt="-a"
-            model="$(
-                find . -mindepth 1 -maxdepth 1 -type f -name "semantic*ade20k*.h5" -print0 \
-                    | head -n 1 -z
-            )"
-            datadir="data/color"
-            ;;
-        pascalvoc)
-            opt="-p"
-            model="$(
-                find . -mindepth 1 -maxdepth 1 -type f -name "semantic*pascalvoc*.h5" -print0 \
-                    | head -n 1 -z
-            )"
-            datadir="data/color"
-            ;;
-    esac
-    perf stat unbuffer \
-        "$pythonpath" src/development/main.py \
-            -k 15 -t 0.995 -s 0.8 -S "$opt" "$model" "$datadir"/*
-fi | filter
+    if [ "$#" -ne 0 ]; then
+        perf stat unbuffer \
+            "$pythonpath" src/development/main.py "$@"
+    else
+        case "$optmodel" in
+            ade20k)
+                opt="-a"
+                model="$(
+                    find . -mindepth 1 -maxdepth 1 -type f -name "semantic*ade20k*.h5" -print0 \
+                        | head -n 1 -z
+                )"
+                datadir="data/color"
+                ;;
+            pascalvoc)
+                opt="-p"
+                model="$(
+                    find . -mindepth 1 -maxdepth 1 -type f -name "semantic*pascalvoc*.h5" -print0 \
+                        | head -n 1 -z
+                )"
+                datadir="data/color"
+                ;;
+        esac
+        perf stat unbuffer \
+            "$pythonpath" src/development/main.py \
+                -k 15 -t 0.995 -s 0.8 -S "$opt" "$model" "$datadir"/*
+    fi | filter
+}
+
+[ "$#" -eq 0 ] && logerrq "no subcommand given"
+subcmd="$1"
+shift
+case "$subcmd" in
+    s|seg|segmentation) segmentation "$@";;
+    *) logerrq "invalid or not-supported subcommand";;
+esac
+
